8fc2f90dcdec99eb3228572ba6cffcd890db9113
Matt Larkin <mlarkin14@gmail.com>
2022-06-13 11:23:08 -0400

    added server and test api routes to get and push

---
+++ server.js
@@ -0,0 +1,20 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+
+const express = require('express');
+const PORT = process.env.PORT || 3010;
+const app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
